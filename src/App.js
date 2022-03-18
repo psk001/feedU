151d178f720cd2ef23c2e538f96dafd7c74b19f8
Pushpak Prateek <56984884+psk001@users.noreply.github.com>
2022-03-18 08:02:51 +0530

    Updated routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
+
 import Header from "./components/Header";
 import Footer from './components/Footer';
 import Home  from './components/Home';
